dfac3c0016abbd2f82f922f05e5b909a4ffc5036
Vitalii Efimov <g3770888@gmail.com>
2022-09-16 11:04:59 -0400

    Selenium day 2 added class CarInsuranceVerification

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/CarInsuranceVerification.java
@@ -0,0 +1,42 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CarInsuranceVerification {
+
+    /*
+    1. Open Chrome browser
+            2. Go to
+            https://www.qa1.excelsoirinsurance.com/login
+            3. Verify URL contains
+                 Expected: "excelsoirinsurance"
+            4. Verify title:
+                   Expected: "Login | Excelsoir Insurance"
+            day2_locators_getText_getAttribute
+
+     */
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.qa1.excelsoirinsurance.com/login");
+        String expected = "xcelsoirinsurance";
+        String actual = driver.getCurrentUrl();
+        if (actual.contains(expected)){
+            System.out.println("URL PASSED!");
+        }else{
+            System.out.println("ERL FAILED!");
+        }
+        expected = "Login | Excelsoir Insurance";
+        actual = driver.getTitle();
+        if (actual.equals(expected)){
+            System.out.println("TITLE PASSED!");
+        }else{
+            System.out.println("TITLE FAILED!");
+        }
+        driver.close();
+    }
+}
